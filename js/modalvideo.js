a27d48d5621d38f4f29693ff8bcc3408794809bd
m4k4rov <makarov.pav.al@gmail.com>
2021-09-22 12:53:05 +0300

    ~modalvideo

--- js/modalvideo.js
+++ js/modalvideo.js
@@ -1,9 +1,19 @@
 const modal = document.getElementById('videoModal');
 const btn = document.querySelector('.play');
 const closebtn = document.querySelector('.videoclose');
+const player = document.getElementById('youtubeVideo');
 
-btn.addEventListener('click',()=> modal.style.display='flex');
-closebtn.addEventListener('click',()=> modal.style.display='none');
+btn.addEventListener('click',()=> {
+    modal.style.display='flex';
+    player.src='https://www.youtube.com/embed/BTkCfdhIggc';
+});
+closebtn.addEventListener('click',()=> {
+    modal.style.display='none';
+    player.src='';
+});
 document.addEventListener('click',event => {
-    if (event.target == modal) modal.style.display='none';
+    if (event.target == modal) {
+        modal.style.display='none';
+        player.src='';
+    };
 });
